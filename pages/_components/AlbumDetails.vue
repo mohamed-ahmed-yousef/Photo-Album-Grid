07c6feaddd3c7832e6bc4fdcdd1899382cdeead9
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-09-30 14:22:03 +0300

    update readme & and use nuxtImage

--- pages/_components/AlbumDetails.vue
+++ pages/_components/AlbumDetails.vue
@@ -30,7 +30,7 @@ defineProps<{
     </div>
     <div class="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid grid-cols-1 gap-4">
         <div v-for="photo in album" :key="photo.id" class="hover:scale-105 overflow-hidden transition-transform duration-300 bg-white rounded-lg shadow-md">
-            <img :src="photo.url" :alt="photo.title" class="object-cover w-full h-48">
+            <NuxtImg  width="600"height="400" :src="photo.url" :alt="photo.title" class="object-cover w-full h-48" />
             <div class="p-4">
                 <h2 class="text-lg font-semibold">{{ photo.title }}</h2>
             </div>
---
+++ pages/_components/ErrorPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen px-0 bg-gray-100">
+        <h1 class="mb-4 text-4xl font-bold text-red-600">Error</h1>
+        <p class="mb-8 text-xl text-gray-600">
+            Oops! Something went wrong.
+        </p>
+        <router-link
+            to="/"
+            class="hover:bg-red-700 px-6 py-3 text-white transition duration-300 bg-red-600 rounded-lg"
+        >
+            Go back to Homepage
+        </router-link>
+    </div>
+</template>
